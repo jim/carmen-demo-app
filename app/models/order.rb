3291a2a895524557b5be3d57c4bef8c5b17c2a77
Jim Benton <jim@autonomousmachine.com>
2012-03-23 17:02:54 -0500

    Add some validations to prevent order saving, and make country_code and state_code accessible so the form behavior is more normal.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ActiveRecord::Base
-  validates :name, presence: true
+  validates_presence_of :name, message: 'is invalid, and this is a demo app and you can\'t save anything anyway.'
+
+  attr_accessible :country_code, :state_code
 end
