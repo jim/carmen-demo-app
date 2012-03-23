5b40d31afbf8c1b8cfc1cbd524991d72701b8ba6
Jim Benton <jim@autonomousmachine.com>
2012-03-23 17:01:16 -0500

    Make orders#new the root route.

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ CarmenTestApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'orders#new'
 
   # See how all your routes lay out with "rake routes"
 
